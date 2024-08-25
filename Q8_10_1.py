bd97dcf38e6181f386012b84a0133987f6d7a013
k-matayosi <n24015@std.it-college.ac.jp>
2024-08-25 18:06:59 +0000

    kadai

---
+++ Q8_10_1.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(filename='practicel.log',level=logging.logging.WARNING("2"))
+logging.warning('1')
+logging.error('2')
+logging.debug('3')
+logging.info('4')
+logging.shutdown()
+
+
+
+2 logging.WARNING
+
